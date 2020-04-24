74c4715a4800a49685d18203629658baa036169f
olivier-boesch <oliv.boesch@gmail.com>
2020-04-24 17:00:57 +0200

    added a key inspector with gui

---
+++ src/key_inspector.py
@@ -0,0 +1,54 @@
+"""
+
+    Affiche interactive
+        Idée Originale: Damien Muti
+        Code : Olivier Boesch
+
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.logger import Logger
+
+
+class KeyboardController(Widget):
+    def __init__(self, app, **kwargs):
+        self.app = app
+        super().__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self.keyboard_callback)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self.keyboard_callback)
+        self._keyboard = None
+
+    def keyboard_callback(self, keyboard, keycode, text, modifiers):
+        self.app.root.ids['display_lbl'].text = "key: \""+keycode[1]+"\""
+        Logger.info("Key pressed: {} {} {}".format(str(keycode), str(text), str(modifiers)))
+
+
+kv_str = """
+BoxLayout:
+    orientation: 'vertical'
+    Label:
+        id : display_lbl
+    Button:
+        size_hint_y: None
+        height: dp(40)
+        text: 'Quit'
+        on_release: app.stop()
+"""
+
+
+class AfficheApp(App):
+
+    def build(self):
+        self.keyboard_controller = KeyboardController(self)
+        root = Builder.load_string(kv_str)
+        return root
+
+
+if __name__ == '__main__':
+    AfficheApp().run()
