494da44d75ef2de996e46b464043f53285d65b34
olivier boesch <oliv.boesch@gmail.com>
2020-05-07 14:14:42 +0200

    added an installation file for the pi3

---
+++ install_kivy_on_rpi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# kivy install on rpi from master
+sudo apt install python3-pip build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
+sudo apt install libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good
+sudo pip3 install ffpyplayer
+sudo pip3 install cython
+sudo pip3 install git+https://github.com/kivy/kivy.git@master
+
